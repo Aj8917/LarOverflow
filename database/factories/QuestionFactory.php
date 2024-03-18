03de059b939846fa1663bc235daccde6183a771d
yogita <yogita@appic.me>
2024-03-18 17:14:16 +0530

    Question table fake data seeeding

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+use Faker\Factory as FakerFactory;
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+
+     protected $model=Question::class;
+    public function definition(): array
+    { 
+        $faker = FakerFactory::create();
+
+        return [
+            'title' => rtrim($faker->sentence($faker->numberBetween(5, 10)), '.'),
+            'body' => $faker->paragraphs($faker->numberBetween(3, 7), true),
+            'views' => $faker->numberBetween(0, 10),
+            'answers' => $faker->numberBetween(0, 10),
+            'votes' => $faker->numberBetween(-3, 10)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\Question;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,9 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(3)->create()->each(function($user) {
+            
+           // For each user, create between 1 and 5 questions
+           Question::factory()->count(random_int(1, 5))->create(['user_id' => $user->id]);
+        });
     }
 }
